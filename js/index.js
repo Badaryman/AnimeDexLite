b77d104d845eca8ee039d1203d523fa5cb6ecb83
Badaryman <154390732+Badaryman@users.noreply.github.com>
2024-01-12 23:32:46 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // Api urls
 
 const proxy = "https://proxy.techzbots1.workers.dev/?u=";
-const IndexApi = "https://api.anime-dex.workers.dev/home";
-const recentapi = "https://api.anime-dex.workers.dev/recent/";
+const IndexApi = "https://api.ralcs.workers.dev/home";
+const recentapi = "https://api.ralcs.workers.dev/recent/";
 
 // Usefull functions
 
